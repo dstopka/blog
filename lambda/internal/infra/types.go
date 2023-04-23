f7a63b8516886c45969498e7ad70385fc1625dbc
Darek Stopka <dariusz.marek.stopka@gmail.com>
2023-04-23 18:15:30 +0200

    Add infrastructure layer

---
+++ lambda/internal/infra/types.go
@@ -0,0 +1,63 @@
+package infra
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+const (
+	QueryParamCursor   = "cursor"
+	QueryParamPageSize = "pageSize"
+	QueryParamSlug     = "slug"
+)
+
+type Lambda interface {
+	ServeLambda(context.Context, events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse
+}
+
+type Post struct {
+	Title         string     `json:"title"`
+	Description   string     `json:"description"`
+	Slug          string     `json:"slug"`
+	Content       string     `json:"content,omitempty"`
+	CoverImageURL string     `json:"coverImageURL"`
+	Tags          []string   `json:"tags,omitempty"`
+	PublishedTime time.Time  `json:"publishedTime"`
+	UpdatedTime   *time.Time `json:"updatedTime,omitempty"`
+}
+
+type PostsPage struct {
+	Posts      []Post `json:"posts"`
+	NextCursor string `json:"nextCursor,omitempty"`
+}
+
+type ErrorResponse struct {
+	Slug        string `json:"slug"`
+	Description string `json:"description,omitempty"`
+	httpStatus  int
+}
+
+func BadRequest(desc string) ErrorResponse {
+	return ErrorResponse{
+		Slug:        "bad-request",
+		Description: desc,
+		httpStatus:  http.StatusBadRequest,
+	}
+}
+
+func InternalServerError() ErrorResponse {
+	return ErrorResponse{
+		Slug:       "internal-server-error",
+		httpStatus: http.StatusInternalServerError,
+	}
+}
+
+func NotFound() ErrorResponse {
+	return ErrorResponse{
+		Slug:       "not-found",
+		httpStatus: http.StatusNotFound,
+	}
+}
