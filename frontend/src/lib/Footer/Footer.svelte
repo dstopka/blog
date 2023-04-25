7d016aba9ef93a34ff682cee069509f31183b50f
Darek Stopka <dariusz.marek.stopka@gmail.com>
2023-04-25 17:04:09 +0200

    Add basic layout with navbar and footer

---
+++ frontend/src/lib/Footer/Footer.svelte
@@ -0,0 +1,46 @@
+
+<footer class="page-footer">
+	<section class="footer-main">
+		<div class="footer-main-wrapper">
+			<a href="https://github.com/dstopka">
+				<i class="fa-brands fa-github" />
+			</a>
+		</div>
+	</section>
+	<section class="footer-legal">
+		&copy; 2023 Dariusz Stopka
+	</section>
+</footer>
+
+<style>
+    .page-footer {
+		color: rgb(252, 252, 252);
+	}
+
+	.footer-main {
+		background-color: rgb(38,38,38);
+		height: 50px;
+		line-height: 50px;
+	}
+
+	.footer-main-wrapper {
+		max-width: 60rem;
+		margin: auto;
+		text-align: center;
+		font-size: 1.5rem;
+		height: 100%;
+	}
+
+	.footer-main-wrapper a {
+		color: inherit;
+		cursor: pointer;
+	}
+
+	.footer-legal {
+		padding: 0 1rem;
+		background-color: rgb(34,34,34);
+		height: 50px;
+		line-height: 50px;
+		font-size: 0.8rem;
+	}
+</style>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Footer from '$lib/Footer/Footer.svelte';
+	import Navbar from '$lib/Navbar/Navbar.svelte';
+</script>
+
+<Navbar />
+<div class="main">
+	<div class="content">
+		<slot />
+	</div>
+</div>
+<Footer />
+
+<style>
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-size: inherit;
+	}
+
+	:root {
+		font-family: 'Rubik', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+		color: rgb(48, 48, 48);
+		line-height: 1.6;
+	}
+
+	:global(body) {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.main {
+		margin-top: 50px;
+		background-color: rgb(252, 252, 252);
+		flex-grow: 1;
+	}
+
+	.content {
+		margin: auto;
+		max-width: 50rem;
+		padding: 0 2rem;
+		/* color: rgb(41, 41, 41); */
+		text-align: justify;
+	}
+</style>
