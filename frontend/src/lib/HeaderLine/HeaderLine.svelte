055e772165d33343d350af8bfe4b2f29eab233fd
Darek Stopka <dariusz.marek.stopka@gmail.com>
2023-05-03 14:43:32 +0200

    Add HeaderLine component

---
+++ frontend/src/lib/HeaderLine/HeaderLine.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let header: string;
+	export let size: string;
+</script>
+
+<div class="header-line">
+	<h2 style="font-size: {size}">{header}</h2>
+	<hr />
+</div>
+
+<style>
+	.header-line {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+        color: var(--color-text-dark-headings);
+	}
+
+	hr {
+		flex-grow: 1;
+		background-color: var(--color-logo-secondary-bg);
+		height: 1px;
+		border: none;
+	}
+</style>
